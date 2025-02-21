facfee6e62ee2ab7274905847d49ff3427d13398
HIT123444 <joshihiteshi126@gmail.com>
2025-02-21 20:31:32 +0530

    PHP

---
+++ project/teacher.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 18, 2025 at 06:47 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `details`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `id` int(9) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `phn_no` bigint(10) NOT NULL,
+  `address` longtext NOT NULL,
+  `qualification` varchar(100) NOT NULL,
+  `join_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`id`, `name`, `email`, `phn_no`, `address`, `qualification`, `join_date`) VALUES
+(1, 'Priti Sikka', 'priti11@gmail.com', 6182129733, 'gurgaon', 'MA B.ed', '2017-05-10'),
+(2, 'rajat', 'rajat@gmail.com', 7392371281, 'jalandhar city', 'MBBS', '2025-02-12'),
+(3, 'hiteshi ', 'hitesho@gmail.com', 12817278223, 'jalandhar', ' mbbs', '0000-00-00'),
+(6, 'pavani joshi', 'pavani11@gmail.com', 9718612786, 'gurgaon, new delhi', ' MA english', '0000-00-00'),
+(7, 'Kartik attri', 'kartik@gmail.com', 9379923125, 'germany', ' B.tech ', '0000-00-00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `teacher`
+--
+ALTER TABLE `teacher`
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
